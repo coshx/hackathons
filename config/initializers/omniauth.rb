49df7e20d7a16eab9fbbb164c6a19ed2f80e6fb5
Ben Taitelbaum <ben@coshx.com>
2012-08-24 18:03:00 -0400

    adds a more aggressive scope
     - https://github.com/intridea/omniauth-github/pull/13

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], :scope => ''
+    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], :scope => 'user'
   else
     # development application
     provider :github, '7f3b06ca4e81ad3e7846', '35dd7f0ec6e5b5ddb652ba87a7fa78503f7ffe16', :scope => 'user'
