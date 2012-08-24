d1e1d6a41c2c84d85e1500d9ae76bc659f40abde
Ben Taitelbaum <ben@coshx.com>
2012-08-24 00:17:38 -0400

    updates with heroku variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '7f3b06ca4e81ad3e7846', '35dd7f0ec6e5b5ddb652ba87a7fa78503f7ffe16', :scope => 'user'
+  if Rails.env.production?
+    provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], :scope => 'user'
+  else
+    # development application
+    provider :github, '7f3b06ca4e81ad3e7846', '35dd7f0ec6e5b5ddb652ba87a7fa78503f7ffe16', :scope => 'user'
+  end
 end
 
 OmniAuth.config.logger = Rails.logger
