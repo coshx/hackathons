ae15226de3d37e885966b7b802ff27ed602decc5
Ben Taitelbaum <ben@coshx.com>
2012-08-24 21:54:40 -0400

    adds last remaining project to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -107,6 +107,8 @@ hackathon.projects << project
 
 project = Project.find_or_create_by_name('Bible Share')
 project.abstract = 'Bibleshare provides an useful tool for online group bible sharing/discussion. One can enter something like John 13:1-17 and the passage will be created. People can post their thoughts and meditations about the scripture. The passage will be visible as people write their thoughts, so they don\'t have to move back and forth between monitor and bible. If one wants to refer to another passage, he can type something like @Rome 3:31 and it will display the verse in the post. People can comment on individual post. All posts will be visible at the same time, so no going back and forth is necessary. A scripture can be either made public for open discussion and sharing, or private for closed group sharing.'
+project.github_url = 'https://github.com/coshx/BibleShare'
+project.url = 'http://bible-share.herokuapp.com/'
 project.save!
 hackathon.projects << project
 
